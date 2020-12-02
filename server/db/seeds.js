93c3daec5b64d0dfd0277c9e9656ad33f15af37f
Ruth Foulis <ruthlilfoulis@gmail.com>
2020-12-02 12:09:34 +0000

    first commit files set up

---
+++ server/db/seeds.js
@@ -0,0 +1,22 @@
+const { Db } = require("mongodb");
+
+use bookings;
+db.dropDatabase();
+
+db.bookings.insertMany([
+    {
+        name: "Ruth Foulis",
+        email: "ruth@ruth.com",
+        checked_in: false 
+    },
+    {
+        name: "Michael Sinclair",
+        email: "michael@michael.ca",
+        checked_in: true
+    },
+    {
+        name: "Father Christmas",
+        email: "nick@northpole.com",
+        checked_in: true
+    }
+]);
